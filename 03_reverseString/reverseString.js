6314bb2281d00dde02f0b43e3b428793be7648d8
Chris <cbrown.cloud@gmail.com>
2025-04-07 10:13:34 -0700

    Make reverse string function

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // store reverse string
+    let fullReverse = "";
 
+    for (let index = string.length - 1; index > -1; index--) {
+        fullReverse += string[index];
+    }
+    return fullReverse;
 };
 
 // Do not edit below this line
