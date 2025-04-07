4ad8bc8a681161857f7c7b715fd90fde8bfce42b
Chris <cbrown.cloud@gmail.com>
2025-04-07 09:58:19 -0700

    Create function that repeats string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    let fullString = "";
+
+        // num should never be less than 0
+        if (num < 0) {
+            return "ERROR";
+        }
+
+        for (let index = 0; index < num; index++) {
+            // Take a string and repeat it num amount of times
+            fullString += string;
+        }
+        return fullString;
 };
 
 // Do not edit below this line
