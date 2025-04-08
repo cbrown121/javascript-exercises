715fdcbb678b377f97fe452bd06a58bee5ea05af
Chris <cbrown.cloud@gmail.com>
2025-04-07 17:19:19 -0700

    Create function that filters an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+// loop through the array
+// compare numbers in the array to the specified number
+// if specified number matches the number in the array, remove it
+
+// Dealing with an unknown amount of arguments
+const removeFromArray = function(arr, ...args) {
+    // filter the array
+    // if the object at the current index pass the filter, add it to the new filter
+    const newArray = arr.filter(item => !args.includes(item));
+    return newArray;
 };
 
 // Do not edit below this line
