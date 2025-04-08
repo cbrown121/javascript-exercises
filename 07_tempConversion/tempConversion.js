ad92c3cfd07ef33b3b22323336754567521a7ec7
Chris <cbrown.cloud@gmail.com>
2025-04-07 18:25:24 -0700

    Create two functions that convert temperatures

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+  return parseFloat(((num - 32) * (5/9)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+// (°F − 32) × 5/9 = °C
+// round to one decimal place
+
+const convertToFahrenheit = function(num) {
+  return parseFloat(((num * (9/5)) + 32).toFixed(1));
 };
+
+//(°C × 9/5) + 32 = °F
+// round to one decimal place
 
 // Do not edit below this line
 module.exports = {
