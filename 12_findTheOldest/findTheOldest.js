9453c0e919ccd01908c52bd95090e0bbecc9a5e7
Chris <cbrown.cloud@gmail.com>
2025-04-11 09:52:45 -0700

    Complete the oldestPerson exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,48 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
+// Need to use reduce. Loop through the array
+// set initial oldest to the first object in the array
+// compare next object to oldest
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        if (!currentPerson.yearOfDeath) {
+            const d = new Date();
+            currentPerson.yearOfDeath = d.getFullYear();
+        } 
 
+        if (!oldest.yearOfDeath) {
+            const d = new Date();
+            oldest.yearOfDeath = d.getFullYear();
+        } 
+
+
+        const oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        const currentPersonAge = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+
+
+
+        if (currentPersonAge > oldestAge) {
+            oldest = currentPerson;
+        }
+
+
+
+        return oldest;
+    });
 };
 
 // Do not edit below this line
